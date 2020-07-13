0be6a7d8b0f9a85ddeea030e485c9eefaa6a32b8
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-07-13 10:17:47 +0200

    v.0.1.2

--- main.js
+++ main.js
@@ -20,3 +20,37 @@ document.getElementById("error").innerHTML = "Du musst eingeben wie der Service 
     document.getElementById("person").innerHTML = "Der Preis pro Person ist: " + person
 }
 
+
+
+
+// function berechnen() {
+//     console.log("test")
+//     let price = document.getElementById("price").value
+//     let person = document.getElementById("person").value
+//     let service = document.getElementById("service").value
+//     console.log(price, person, service)
+//     if (service == 'notSelected') {
+//         document.getElementById("alert").innerHTML = "Du musst angeben wie war der Service"
+//     } else if (service == 'good') {
+//         document.getElementById("alert").innerHTML = ''
+//         document.getElementById('trink').innerHTML = `Trinkgeld ist: ${Number(price) * 0.2}`
+//         document.getElementById('total').innerHTML = `Gesamtsumme ist :${Number(price) + (Number(price) * 0.2)}`
+//         document.getElementById("pro-person").innerHTML = `pro person :${((Number(price) + (Number(price) * 0.2)) / Number(person)).toFixed(2)}`
+
+//     } else if (service == 'okay') {
+//         document.getElementById("alert").innerHTML = ''
+//         document.getElementById('trink').innerHTML = `Trinkgeld ist: ${Number(price) * 0.1}`
+//         document.getElementById('total').innerHTML = `Gesamtsumme ist :${Number(price) + (Number(price) * 0.1)}`
+//         document.getElementById("pro-person").innerHTML = `pro person :${((Number(price) + (Number(price) * 0.1)) / Number(person)).toFixed(2)}`
+
+//     } else if (service == 'bad') {
+//         document.getElementById("alert").innerHTML = ''
+//         document.getElementById('trink').innerHTML = `Trinkgeld ist: ${Number(price) * 0.02}`
+//         document.getElementById('total').innerHTML = `Gesamtsumme ist :${Number(price) + (Number(price) * 0.02)}`
+//         document.getElementById("pro-person").innerHTML = `pro person :${((Number(price) + (Number(price) * 0.02)) / Number(person).toFixed(2))}`
+
+//     }
+
+// }
+
+
